e24e2d853e939c6f26e591d123bbf64b519dd01a
Caio Abner de Oliveira Santos <129506960+CaioAbner@users.noreply.github.com>
2023-11-23 16:42:39 -0300

    Add files via upload

---
+++ teeste cópia sem express/client/src/pages/IndexPage.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+import Header from '../Header';
+
+export default function IndexPage() {
+    return (
+        <div>
+            página principal aqui
+        </div>
+    );
+}
